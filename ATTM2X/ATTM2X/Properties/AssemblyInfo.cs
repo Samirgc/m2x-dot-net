16dd97d6bc4dba1d1adc61f9d5cfb65410bb4dc1
Kelvin McDaniel <kelvin.mcdaniel@live.com>
2016-10-01 11:13:15 -0400

    Minor version bump for new release packaging.

--- ATTM2X/ATTM2X/Properties/AssemblyInfo.cs
+++ ATTM2X/ATTM2X/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ATTM2X")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AT&T's M2X .NET Client")]
+[assembly: AssemblyDescription("The AT&T M2X API provides all the needed operations and methods to connect your devices to AT&T's M2X service.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AT&T IoT Services")]
 [assembly: AssemblyProduct("ATTM2X")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
